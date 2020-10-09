221f5a4c14781a0a1e61348c78767d2ff9721d2a
coleus99 <christopherjohncoles@gmail.com>
2020-10-09 20:58:24 +0100

    setup html and css plus function to create book objects still need to setup form to pass input to function

---
+++ script.js
@@ -0,0 +1,48 @@
+let library= [];
+
+function createBook(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read){
+    library.push(new createBook(title, author, pages, read));
+}
+
+function display(){
+    for (let item of library){
+        let book = document.createElement('div');
+        book.classList.add('card');
+        book.innerHTML=
+            '<img src="https://picsum.photos/300" class="card-img-top" alt="...">' +
+            '<div class="card-body">' +
+                `<h5 class="card-title">${item.title}</h5>` +
+                `<p class="card-text">${item.author}</p>` +
+                `<p class="card-text">${item.pages} Pages</p>` +
+                '<div class="btn-group">' +
+                    `<a href="#" class="btn btn-sm btn-info">Mark as ${item.read? 'unread' : 'read'}</a>` +
+                    '<a href="#" class="btn btn-sm btn-danger">Delete</a>' +
+                '</div>' +
+            '</div>';
+            if(item.read){
+                completedBooks.appendChild(book);
+            }
+            else{
+                readingList.appendChild(book);
+            }
+    }
+}
+
+let readingList = document.querySelector('.readingList');
+let completedBooks = document.querySelector('.completedBooks');
+let formSubmit = document.querySelector('#addNew button[type="Submit"]');
+// formSubmit.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     console.log(event);
+// });
+
+addBookToLibrary('Ramble Book', 'Adam Buxton', '398', false);
+addBookToLibrary('The Watchmaker of Filigree Street', 'Natasha Pulley', '587', true);
+display();
