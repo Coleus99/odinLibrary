0f7884c6a58a16a586f0fa65d8f462a0a2afb294
coleus99 <christopherjohncoles@gmail.com>
2020-10-12 12:36:39 +0100

    added functionality to 'read' and 'delete buttons and updated styling on cards container

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// setup object to store books and method to add them
+
 let library= [];
 
 function createBook(title, author, pages, read){
@@ -5,49 +7,74 @@ function createBook(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.imgID = parseInt(Math.random()*1000);
 }
 
 function addBookToLibrary(title, author, pages, read){
     library.push(new createBook(title, author, pages, read));
 }
 
-function display(){
-    completedBooks.innerHTML='';
-    readingList.innerHTML='';
-    for (let item of library){
-        let book = document.createElement('div');
-        book.classList.add('card');
-        book.innerHTML=
-            `<img src="https://picsum.photos/id/${parseInt(Math.random()*1000)}/300" class="card-img-top" alt="...">` +
-            '<div class="card-body">' +
-                `<h5 class="card-title">${item.title}</h5>` +
-                `<p class="card-text">${item.author}</p>` +
-                `<p class="card-text">${item.pages} Pages</p>` +
-                '<div class="btn-group">' +
-                    `<a href="#" class="btn btn-sm btn-info">Mark as ${item.read? 'unread' : 'read'}</a>` +
-                    '<a href="#" class="btn btn-sm btn-danger">Delete</a>' +
-                '</div>' +
-            '</div>';
-            if(item.read){
-                completedBooks.appendChild(book);
-            }
-            else{
-                readingList.appendChild(book);
-            }
-    }
-}
+// user input to add new books
 
 let readingList = document.querySelector('.readingList');
 let completedBooks = document.querySelector('.completedBooks');
 let form = document.querySelector('#addNew form');
+let radio;
+
 form.addEventListener('submit', function(event){
-    console.log(event);
-    addBookToLibrary(event.target[0].value,event.target[1].value,event.target[2].value,event.target[3].value)
+    radio = document.querySelector('input[name="customRadio"]:checked');
+    addBookToLibrary(event.target[0].value,event.target[1].value,event.target[2].value,radio.value)
     display();
     form.reset();
     event.preventDefault();
 });
 
-addBookToLibrary('Ramble Book', 'Adam Buxton', '398', false);
-addBookToLibrary('The Watchmaker of Filigree Street', 'Natasha Pulley', '587', true);
+// output to page
+
+function display(){
+    completedBooks.innerHTML='';
+    readingList.innerHTML='';
+    for (let item of library){
+        let book = document.createElement('div');
+        book.classList.add('col-md-4', 'col-sm-6');
+        book.innerHTML=
+        `<div class="card">` +
+            `<img src="https://picsum.photos/id/${item.imgID}/300" class="card-img-top" alt="...">` +
+            '<div class="card-body">' +
+                `<h5 class="card-title">${item.title}</h5>` +
+                `<p class="card-text">${item.author}</p>` +
+                `<p class="card-text">${item.pages} Pages</p>` +
+                '<div class="btn-group">' +
+                    `<a href="#" id="toggleRead" class="btn btn-sm btn-info">Mark as ${item.read? 'unread' : 'read'}</a>` +
+                    '<a href="#" id="delete" class="btn btn-sm btn-danger">Delete</a>' +
+                '</div>' +
+            '</div>'
+        '</div>';
+        if(item.read){
+            completedBooks.appendChild(book);
+        }
+        else{
+            readingList.appendChild(book);
+        }
+        book.addEventListener('click', function(event){
+            if (event.target.id==="toggleRead"){
+                item.read = ! item.read;
+                display();
+            }
+            else if (event.target.id==="delete"){
+                library.splice(library.indexOf(item), 1);
+                display();
+            }
+        })
+    }
+}
+
+//add some demo content 
+addBookToLibrary('Ramble Book', 'Adam Buxton', '363', false);
+addBookToLibrary('1984', 'George Orwell', '280', false);
+addBookToLibrary('Brave New World', 'Aldous Huxley', '248', true);
+addBookToLibrary('My Bookey Wook', 'Russell Brand', '432', false);
+addBookToLibrary('The Watchmaker of Filigree Street', 'Natasha Pulley', '487', true);
 display();
+
+//Need to fix top nav so that anchor links are offset and collapses for mobiles.
